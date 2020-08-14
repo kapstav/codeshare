5d863608a2fb0ee8cd66b3bb47436626c50bfa54
Kapil Srivastava <ekapstav@msn.com>
2020-08-13 23:06:57 -0700

    Exception rephrase wordings

--- consumer/KapsConsumerWeb.py
+++ consumer/KapsConsumerWeb.py
@@ -31,7 +31,7 @@ def KapsConsumerWeb(ca_path, cert_path, key_path, db_cred):
     try:
         db_conn = psycopg2.connect(db_uri)
     except (psycopg2.OperationalError) as e:
-        print("-->Mobile Consumer::Unable to connect to a kafka broker, or the service is shutdown. Check the service_uri value in config.ini<--");
+        print("-->Mobile Consumer::Unable to connect to a postgresql, or the service is shutdown. Check the service_uri value in config.ini<--");
         sys.exit(1);
 
     #opening a cursor with dictionary objects as output
